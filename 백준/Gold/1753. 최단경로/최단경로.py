6bdfbf888c8430d57a18dc1966f487bf80cc8af9
jinwooiminha <134275738+jinwooiminha@users.noreply.github.com>
2023-11-03 22:41:54 +0900

    [Gold IV] Title: 최단경로, Time: 476 ms, Memory: 135408 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,46 +1,37 @@
-# 다익스트라 알고리즘
-import sys
 import heapq
+import sys
+
+
+INF = sys.maxsize
+
+input = sys.stdin.readline
+
 V,E = map(int,input().split())
-graph = {i:[] for i in range(1,V+1)}
 start = int(input())
 
-def inp():
-  #return input()
-  return sys.stdin.readline()
+graph = {i:[] for i in range(1,V+1)}
 
 for i in range(E):
-  a,b,c = map(int,inp().split())
-  graph[a].append((c,b))
-    
-  
+  u,v,w = map(int,input().split())
+  graph[u].append((w,v))
+
 
 def dijikstra(graph,start):
-  distance_list = [sys.maxsize for i in range(1 + len(graph.keys()))]
-  distance_list[start] = 0
+  distance = [INF for i in range(1 + len(graph.keys()))]
+  distance[start] = 0
   location = start
   hp = []
   heapq.heappush(hp,(0,start))
   while hp:
-    distance,location  = heapq.heappop(hp)
-    if distance_list[location] < distance:
+    dist,location  = heapq.heappop(hp)
+    if distance[location] < dist:
       continue
-
     for weight in graph[location]:
-        cost = distance_list[location]+weight[0]
-        if cost < distance_list[weight[1]]:                                     
-            distance_list[weight[1]] = cost
-            heapq.heappush(hp,(cost,weight[1]))
-  
-  return distance_list
-        
-
-    
-
-    
-
-
-
+      cost = weight[0] + distance[location]
+      if cost < distance[weight[1]]:
+        distance[weight[1]] = cost
+        heapq.heappush(hp,(cost,weight[1]))
+  return distance
 
 
 if V == 1:
@@ -48,7 +39,7 @@ if V == 1:
 else:
   L = dijikstra(graph,start)
   for i in range(1,len(L)):
-    if L[i] == sys.maxsize:
+    if L[i] == INF:
       print('INF')
     else:
       print(L[i])
